e9a513776c9f4f4d20a71407020c3e873570be68
nimaytenzin <nimaytenzin@gmail.com>
2025-09-04 09:24:55 +0600

    ok

--- src/app/presentations/public/booking/public-select-seats/public-select-seats.component.ts
+++ src/app/presentations/public/booking/public-select-seats/public-select-seats.component.ts
@@ -1303,15 +1303,6 @@ export class PublicSelectSeatsComponent implements OnInit, OnDestroy {
 								closable: false,
 								dismissableMask: false,
 								data: bookingData,
-								width: '100vw',
-								height: '100vh',
-								contentStyle: {
-									'max-height': '100vh',
-									'overflow-y': 'auto',
-									padding: '0',
-									'border-radius': '0',
-								},
-								styleClass: 'payment-modal-mobile',
 							});
 
 							// Listen for booking completion
