7505c4e4882d1f7f87851e959602e98e0e8e538f
nimaytenzin <nimaytenzin@gmail.com>
2025-09-04 09:48:13 +0600

    OK"

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import {
+	provideRouter,
+	withEnabledBlockingInitialNavigation,
+	withInMemoryScrolling,
+} from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
@@ -11,7 +15,14 @@ import { authInterceptor } from './app/core/interceptors/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
 	providers: [
-		provideRouter(routes), // Provide the routes
+		provideRouter(
+			routes,
+			withInMemoryScrolling({
+				scrollPositionRestoration: 'top', // Scrolls to top on navigation
+				anchorScrolling: 'enabled', // Allows scrolling to anchors (#id)
+			}),
+			withEnabledBlockingInitialNavigation() // Optional: blocks initial navigation until app is ready
+		), // Provide the routes with scroll configuration
 		provideHttpClient(withInterceptors([authInterceptor])), // Provide HttpClient with functional interceptor
 		provideAnimationsAsync(),
 		providePrimeNG({
