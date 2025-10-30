a8f3c4bf3c70add975c255b099a8a2a1a80db1e5
nimaytenzin <nimaytenzin@gmail.com>
2025-10-30 17:12:27 +0600

    fix styling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	production: false,
+	production: false, // Should be false for development environment
 	BASEAPI_URL: 'http://localhost:3000',
 	APPNAME: 'NSDF',
 	APPSLOGAN: 'National Sampling and Frame Dashboard',
