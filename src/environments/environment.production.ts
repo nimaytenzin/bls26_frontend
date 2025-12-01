62694e0e503eea3cc631b718ef61f09a183d00bc
nimaytenzin <nimaytenzin@gmail.com>
2025-12-01 20:11:24 +0600

    added prod env

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: true,
 	APPNAME: 'NSDF',
 	APPSLOGAN: 'National Sampling and Frame Dashboard',
-	BASEAPI_URL: 'https://nsfd.ddnsfree.com/api',
+	BASEAPI_URL: 'https://nsfd-bsds.nsb.gov.bt/api',
 };
