5b11bf29312e6b5f6eeb552c9c9ecd9dae6996a4
nimaytenzin <nimaytenzin@gmail.com>
2025-12-10 15:34:24 +0600

    updated production environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,6 +2,6 @@ export const environment = {
 	production: true,
 	APPNAME: 'NSDF',
 	APPSLOGAN: 'National Sampling and Frame Dashboard',
-	BASEAPI_URL: 'https://nsfd.ddnsfree.com/api',
+	BASEAPI_URL: 'https://nsfd-bsds.nsb.gov.bt/api',
 	KMLPARSER_URL: 'http://localhost:8000',
 };
