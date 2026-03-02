eea38b8110873a67099fd60110836df02ba140cd
nimaytenzin <nimaytenzin@gmail.com>
2026-03-02 09:47:53 +0600

    ok

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,6 +2,5 @@ export const environment = {
 	production: true,
 	APPNAME: 'BLS 2026',
 	APPSLOGAN: 'Bhutan Living Standard 2026',
-	BASEAPI_URL: 'https://nsfd-bsds.nsb.gov.bt/api',
-	KMLPARSER_URL: 'https://nsfd-bsds.nsb.gov.bt/automation',
+	BASEAPI_URL: 'https://bls26.ddnsfree.com/api',
 };
